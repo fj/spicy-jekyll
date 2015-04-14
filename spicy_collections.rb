a2ab28567e2b7f5b1573475ce792b10d0946a7be
John Feminella <jxf@jxf.me>
2015-04-14 18:27:30 -0400

    Be more precise about using metadata fields

--- spicy_collections.rb
+++ spicy_collections.rb
@@ -27,10 +27,24 @@ module Jekyll
     end
 
     def assign_navigation_links(collection)
-      sort_field = collection.metadata.fetch 'sort_by', 'date'
-      collection.docs.sort_by { |d| d.data.fetch(sort_field) }.each_cons(2) do |d1, d2|
+      sorted_collection(collection).each_cons(2) do |d1, d2|
         d2.data['previous'] = navigation_hash_for_document d1
         d1.data['next']     = navigation_hash_for_document d2
+      end
+    end
+
+    def sorted_collection(collection)
+      sort_field = collection.metadata.fetch 'sort_by', 'date'
+      collection_sorted_by_field(collection, sort_field)
+    end
+
+    def collection_sorted_by_field(collection, field)
+      if collection.docs.all? { |d| !!d.data[field] }
+        Jekyll.logger.info "sorting by data field: #{field}"
+        collection.docs.sort_by { |d| d.data.fetch(field) }
+      else
+        Jekyll.logger.info "sorting by object field: #{field}"
+        collection.docs.sort_by(&:"#{field}")
       end
     end
 
