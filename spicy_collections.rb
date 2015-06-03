24733039dbb3e6a9a6a9f8641d787763582c2ed1
John Feminella <jxf@jxf.me>
2015-06-03 10:49:04 -0400

    Assign asset paths and document ids

--- spicy_collections.rb
+++ spicy_collections.rb
@@ -1,6 +1,6 @@
 module Jekyll
   class SpicyCollections < Generator
-    VERSION = '0.0.1'
+    VERSION = '0.0.2'
     safe true
     priority :high
 
@@ -9,27 +9,48 @@ module Jekyll
         Jekyll.logger.info "found metadata for collection #{name}: #{collection.metadata}"
         generate_permalinks name, collection
         assign_navigation_links collection
+        assign_asset_paths name, collection
       end
     end
 
     def generate_permalinks(collection_name, collection)
-      refname = collection.metadata.fetch 'refname'
-      r = %r{^(.*)-?#{refname}-(.*)$}
-
       collection.docs.each do |d|
-        _, doc_id, doc_title = r.match(d.basename_without_ext).to_a
+        refname = refname_for(collection)
+        _, doc_id, doc_title = collection_segments_for(d, refname)
         raise ArgumentError, "couldn't find a title" unless doc_title
 
         constructed_slug = doc_title
         Jekyll.logger.info "writing slug for #{refname}: #{constructed_slug}"
         d.data['permalink'] = "/#{collection_name}/#{constructed_slug}/"
+
+        if !doc_id.empty?
+          Jekyll.logger.info "writing document id for #{d.basename_without_ext}: #{collection_name}-#{doc_id}"
+          d.data['doc_id'] = "#{collection_name}-#{doc_id}"
+        end
       end
+    end
+
+    def refname_for(collection)
+      collection.metadata.fetch 'refname'
+    end
+
+    def collection_segments_for(document, refname)
+      r = %r{^(.*?)-?#{refname}-(.*)$}
+      r.match(document.basename_without_ext).to_a
     end
 
     def assign_navigation_links(collection)
       sorted_collection(collection).each_cons(2) do |d1, d2|
         d2.data['previous'] = navigation_hash_for_document d1
         d1.data['next']     = navigation_hash_for_document d2
+      end
+    end
+
+    def assign_asset_paths(collection_name, collection)
+      collection.docs.each do |d|
+        _, doc_id, doc_title = collection_segments_for(d, refname_for(collection))
+        d.data['image_path'] = "/images/#{collection_name}/#{doc_id}"
+        Jekyll.logger.info "adding image path for #{d.path}: #{d.data['image_path']}"
       end
     end
 
